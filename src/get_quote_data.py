a91875802f73a635f7929cf5107bdd2a00cb1950
Mate Balogh <mb755@cam.ac.uk>
2024-08-13 11:14:03 +0100

    refined output file format to save each ticker as a separate table in the same file

--- src/get_quote_data.py
+++ src/get_quote_data.py
@@ -1,9 +1,10 @@
 import configparser as cfg
 import os
 
+# import pandas as pd
+
 from utils.config_parser import default_parser
 from utils.alpaca_data import get_active_assets
-from utils.math import convert_size
 
 from alpaca.trading.client import TradingClient
 from alpaca.data.historical import StockHistoricalDataClient
@@ -11,6 +12,7 @@ from alpaca.data.requests import StockQuotesRequest
 
 from datetime import datetime, date, time
 from pytz import timezone
+from tqdm import tqdm
 
 root_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 
@@ -42,44 +44,50 @@ secret_key = config.get("alpaca", "secret_key")
 trading_client = TradingClient(api_key=api_key, secret_key=secret_key)
 
 assets_df = get_active_assets(trading_client)
+all_stocks = assets_df["symbol"].tolist()
 
-# switch to all assets
-tickers = ["HD", "WMT", "TSLA"]
+# CR TODO: eventually switch to all assets
+tickers = ["EGRX", "HD", "WMT", "TSLA"]
 
 nyc = timezone("US/Eastern")
 
 marketdata_client = StockHistoricalDataClient(api_key=api_key, secret_key=secret_key)
 
+# CR TODO: get a date or daterange from command line
 date = date(2024, 2, 5)
 date_str = date.strftime("%Y-%m-%d")
 
-request_params = StockQuotesRequest(
-    symbol_or_symbols=tickers,
-    # these times are in UTC, this loads in a full day of ticks
-    start=nyc.localize(datetime.combine(date, time(3, 50))),
-    end=nyc.localize(datetime.combine(date, time(21, 10))),
-)
+output_filename = f"{root_dir}/output/quote_data_{date_str}{output_suffix}.h5"
 
-result = marketdata_client.get_stock_quotes(request_params)
+# if output already exists, print a warning and exit
+if os.path.exists(output_filename):
+    print(f"Output file {output_filename} already exists. Exiting.")
+    exit(1)
 
-result_df = result.df
+for ticker in (pbar := tqdm(tickers)):
+    pbar.set_description(f"Processing {ticker}")
 
-# flatten conditions, so the result can be serialized
-result_df["conditions"] = result_df["conditions"].apply(lambda x: ",".join(x))
+    request_params = StockQuotesRequest(
+        symbol_or_symbols=tickers,
+        # these times are in UTC, this loads in a full day of ticks
+        start=nyc.localize(datetime.combine(date, time(3, 50))),
+        end=nyc.localize(datetime.combine(date, time(21, 10))),
+    )
 
-print(result_df.head())
+    result = marketdata_client.get_stock_quotes(request_params)
 
-pandas_memory_usage = result_df.memory_usage(index=True).sum()
+    result_df = result.df
 
-print(f"DataFrame usage: {convert_size(pandas_memory_usage)}")
+    # flatten conditions, so the result can be serialized
+    result_df["conditions"] = result_df["conditions"].apply(lambda x: ",".join(x))
 
-result_df.to_hdf(
-    f"{root_dir}/output/quote_data_{date_str}{output_suffix}.h5",
-    key="data",
-    mode="w",
-    format="table",
-    complevel=9,
-    complib="blosc",
-)
+    result_df.to_hdf(
+        output_filename,
+        key=ticker,
+        mode="a",
+        format="table",
+        complevel=9,
+        complib="blosc",
+    )
 
 # maybe save different assets in different tables, with key=ticker
